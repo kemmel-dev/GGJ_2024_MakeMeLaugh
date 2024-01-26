145d72cc26fe9013c62fba0844335cfbbbd8c62b
Thomas Haagsma <t_haagsma1998@hotmail.com>
2024-01-26 23:48:32 +0100

    Initial scene structure

---
+++ GGJ_2024_MakeMeLaugh/Assets/FartyMcFartFace/ButtonMashController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonMashController : MonoBehaviour
+{
+    public TextMeshProUGUI buttonMashTestCounterUGUI;
+    private int amountOfButtonMashes = 0;
+
+    private void Awake()
+    {
+        
+    }
+}
