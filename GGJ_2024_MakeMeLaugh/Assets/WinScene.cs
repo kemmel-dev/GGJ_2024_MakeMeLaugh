bf9f38049156383fbc5ac2251584da69a0c9f6ab
Kamiel de Visser <kamieldevisser@gmail.com>
2024-01-27 19:02:10 +0100

    add WinScene

--- GGJ_2024_MakeMeLaugh/Assets/WinScene.cs
+++ GGJ_2024_MakeMeLaugh/Assets/WinScene.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class WinScene : MonoBehaviour
@@ -9,15 +10,20 @@ public class WinScene : MonoBehaviour
     public List<GameObject> thrones;
     public List<GameObject> players;
 
+    public TextMeshProUGUI winText;
+
     private void Start()
     {
-        WinForPlayer(
-            GameManager.Instance.Players.OrderByDescending(player => player.PlayerData.points).First().PlayerIndex
-        );
+        // var winningPlayer = GameManager.Instance.Players.OrderByDescending(player => player.PlayerData.points).First();
+        var winningPlayer = GameManager.Instance.Players[1];
+        Debug.Log(winningPlayer.PlayerIndex);
+        Debug.Log(winningPlayer.PlayerData.color);
+        WinForPlayer(winningPlayer.PlayerIndex, winningPlayer.PlayerData.color);
     }
 
-    public void WinForPlayer(int num)
+    public void WinForPlayer(int num, Color color)
     {
+        winText.color = color;
         for (int i = 0; i < 4; i++)
         {
             if (num == i) continue;
