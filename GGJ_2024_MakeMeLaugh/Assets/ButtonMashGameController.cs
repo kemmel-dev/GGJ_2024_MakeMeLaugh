86412d70dd388e1aa9f1b351562c7ccb77f5038a
Thomas Haagsma <t_haagsma1998@hotmail.com>
2024-01-27 13:52:10 +0100

    commit before merge of input

--- GGJ_2024_MakeMeLaugh/Assets/ButtonMashGameController.cs
+++ GGJ_2024_MakeMeLaugh/Assets/ButtonMashGameController.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ButtonMashGameController : MonoBehaviour
 {
     public MiniGameController miniGameController;
-    public List<ButtonMashController> buttonMashPlayers = new List<ButtonMashController>();
+    public List<GameObject> buttonMashPlayers = new List<GameObject>();
     public TextMeshProUGUI buttonMashTestCounterUGUI;
+    public TextMeshProUGUI countdownUi;
     public int countdownAmount;
-    public int buttonMashTimerAmount; 
-    
+    public int buttonMashTimerAmount;
+
+    private int previousPlayerScore;
+    private ButtonMashController previousPlayer;
 
     private void Start()
     {
         miniGameController = FindObjectOfType<MiniGameController>();
 
+        foreach (var buttonMashPlayer in miniGameController.PlayerObjects/*FindObjectsOfType<ButtonMashController>()*/)
+        {
+            buttonMashPlayers.Add(buttonMashPlayer.gameObject);
+        }
+
         StartCoroutine(CountdownForStartTimer(countdownAmount));
-        StartCoroutine(ButtonMeshTimer(buttonMashTimerAmount));
     }
 
     // Replace with Timer from GameController when available
@@ -29,11 +38,18 @@ public class ButtonMashGameController : MonoBehaviour
         while (timer > 0)
         {
             Debug.Log(timer);
+            countdownUi.text = timer.ToString();
             yield return new WaitForSeconds(1);
             timer--;
         }
 
-        SelectWinner();
+        countdownUi.text = "Go!";
+
+        ToggleCanMash();
+        StartCoroutine(ButtonMeshTimer(buttonMashTimerAmount));
+
+        yield return new WaitForSeconds(0.5f);
+        countdownUi.gameObject.SetActive(false);
     }
 
     public IEnumerator ButtonMeshTimer(int buttonMashTimerAmount)
@@ -52,10 +68,52 @@ public class ButtonMashGameController : MonoBehaviour
     public void SelectWinner()
     {
         // Code that checks which player has the most button mashes
-        buttonMashPlayers = buttonMashPlayers.OrderByDescending(player => player.amountOfButtonMashes).ToList();
+        ToggleCanMash();
 
-        buttonMashTestCounterUGUI.text = "Player " + buttonMashPlayers[0] + " Wins, with " + buttonMashPlayers[0].amountOfButtonMashes + " buttons mashes";
+        buttonMashPlayers = buttonMashPlayers.OrderByDescending(player => player.GetComponent<ButtonMashController>().amountOfButtonMashes).ToList();
 
-        // Add logic for which player gets 3, 2, 1, and 0 points for this round and load scoreboard scene
+        buttonMashTestCounterUGUI.text = "Player 1: " + buttonMashPlayers[0].GetComponent<ButtonMashController>().amountOfButtonMashes + 
+                                         " Player 2: " + buttonMashPlayers[1].GetComponent<ButtonMashController>().amountOfButtonMashes +
+                                         " Player 3: " + buttonMashPlayers[2].GetComponent<ButtonMashController>().amountOfButtonMashes +
+                                         " Player 4: " + buttonMashPlayers[3].GetComponent<ButtonMashController>().amountOfButtonMashes;
+
+
+
+       /* // Add logic for which player gets 3, 2, 1, and 0 points for this round and load scoreboard scene
+        foreach (var player in GameManager.Instance.Players) 
+        {
+            //int playerScore = buttonMashPlayers[i].GetComponent;
+        
+
+        }
+
+        for (int i = 0; i < buttonMashPlayers.Count; i++)
+        {
+            buttonMashPlayers[i].GetComponent<ButtonMashController>().possiblePlaces = (ButtonMashController.PossiblePlaces)i;
+
+            int playerScore = buttonMashPlayers[i].GetComponent<ButtonMashController>().amountOfButtonMashes;
+
+            if (playerScore == previousPlayerScore)
+            {
+                buttonMashPlayers[i].GetComponent<ButtonMashController>().possiblePlaces = previousPlayer.possiblePlaces;
+            }
+
+            previousPlayer = buttonMashPlayers[i].GetComponent<ButtonMashController>();
+            previousPlayerScore = playerScore;
+
+
+
+            buttonMashPlayers[i].GetComponent<ButtonMashController>().SetPlace();
+        }*/
+
+    }
+
+
+    public void ToggleCanMash()
+    {
+        foreach (var buttonMashPlayer in buttonMashPlayers)
+        {
+            buttonMashPlayer.GetComponent<ButtonMashController>().canMash = !buttonMashPlayer.GetComponent<ButtonMashController>().canMash;
+        }
     }
 }
--- GGJ_2024_MakeMeLaugh/Assets/FartyMcFartFace/ButtonMashController.cs
+++ GGJ_2024_MakeMeLaugh/Assets/FartyMcFartFace/ButtonMashController.cs
@@ -7,7 +7,17 @@ using UnityEngine.InputSystem;
 public class ButtonMashController : MiniGamePlayerController
 {
     public int amountOfButtonMashes = 0;
+    public bool canMash;
+  /*  public enum PossiblePlaces
+    {
+        FIRST,
+        SECOND,
+        THIRD,
+        LAST
+    }
 
+    public PossiblePlaces possiblePlaces;
+*/
     public override void Initialize(PlayerController playerController)
     {
         base.Initialize(playerController);
@@ -17,7 +27,7 @@ public class ButtonMashController : MiniGamePlayerController
 
     private void ButtonMashController_SouthButton(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        if (canMash && ctx.performed)
         {
             ButtonMashed();
         }
@@ -27,7 +37,29 @@ public class ButtonMashController : MiniGamePlayerController
     {
         amountOfButtonMashes++;
     }
+/*
+    public void SetPlace()
+    {
+            switch (possiblePlaces)
+            {
+                case PossiblePlaces.FIRST:
+                    PlayerControllerReference.PlayerData.pointsThisRound = 3;
+                    break;
 
+                case PossiblePlaces.SECOND:
+                    PlayerControllerReference.PlayerData.pointsThisRound = 2;
+                    break;
+
+                case PossiblePlaces.THIRD:
+                    PlayerControllerReference.PlayerData.pointsThisRound = 1;
+                    break;
+
+                case PossiblePlaces.LAST:
+                    PlayerControllerReference.PlayerData.pointsThisRound = 0;
+                    break;
+            }
+    }
+*/
     private void OnDestroy()
     {
         PlayerControllerReference.SouthButton -= ButtonMashController_SouthButton;
