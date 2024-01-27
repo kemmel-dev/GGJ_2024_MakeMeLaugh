6d0e112a8189ede290c7d55d45dfc1246e1aee76
Kamiel de Visser <kamieldevisser@gmail.com>
2024-01-27 19:23:10 +0100

    restart game

--- GGJ_2024_MakeMeLaugh/Assets/WinScene.cs
+++ GGJ_2024_MakeMeLaugh/Assets/WinScene.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class WinScene : MonoBehaviour
 {
@@ -21,6 +23,14 @@ public class WinScene : MonoBehaviour
         WinForPlayer(winningPlayer.PlayerIndex, winningPlayer.PlayerData.color);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnReloadGame();
+        }
+    }
+
     public void WinForPlayer(int num, Color color)
     {
         winText.color = color;
@@ -30,5 +40,13 @@ public class WinScene : MonoBehaviour
             thrones[i].gameObject.SetActive(false);
             players[i].gameObject.SetActive(false);
         }
+    }
+
+    public void OnReloadGame()
+    {
+        GameManager.Instance.Players.ForEach(controller => Destroy(controller.gameObject));
+        Destroy(GameManager.Instance.gameObject);
+        GameManager.Instance = null;
+        SceneManager.LoadScene("JoinScene");
     }
 }
