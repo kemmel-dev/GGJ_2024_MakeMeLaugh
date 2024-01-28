2f437bd303ebf482ffa21dfe062ec723c6472648
Feskor <patrick.yel@hotmail.com>
2024-01-28 13:05:22 +0100

    commit

--- GGJ_2024_MakeMeLaugh/Assets/GetTriggerFingered/Scripts/GetTriggerFingeredManager.cs
+++ GGJ_2024_MakeMeLaugh/Assets/GetTriggerFingered/Scripts/GetTriggerFingeredManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GetTriggerFingeredManager : MonoBehaviour
 {
@@ -11,31 +12,45 @@ public class GetTriggerFingeredManager : MonoBehaviour
     private void Start()
     {
         audioData = GetComponent<AudioSource>();
+        PlayerScoreTexts[0].color = FindObjectOfType<PlayerData>().Colors[0];
+        PlayerScoreTexts[1].color = FindObjectOfType<PlayerData>().Colors[1];
+        PlayerScoreTexts[2].color = FindObjectOfType<PlayerData>().Colors[2];
+        PlayerScoreTexts[3].color = FindObjectOfType<PlayerData>().Colors[3];
+
     }
     public TextMeshProUGUI timerText;
     public List<TextMeshProUGUI> PlayerScoreTexts;
     public TextMeshProUGUI winnerText;
+
+    public GameObject Lt;
+    public GameObject rt;
     Dictionary<PlayerController, int> scores = new();
     public void AddScore(PlayerController player)
     {
         scores.TryAdd(player, scores.Count);
         if(scores.Count == 4)
         {
-            winnerText.gameObject.SetActive(true);
-            winnerText.text = "Player " + (player.PlayerIndex + 1) + " won!";
+            //winnerText.gameObject.SetActive(true);
+            //winnerText.text = "Player " + (player.PlayerIndex + 1) + " won!";
             GameManager.Instance.SetScorePerPlayer(scores);
 
-            foreach(TextMeshProUGUI text in PlayerScoreTexts)
-            {
-                text.gameObject.SetActive(false);
-            }
+            //foreach (TextMeshProUGUI text in PlayerScoreTexts)
+            //{
+            //    text.gameObject.SetActive(false);
+            //}
             StartCoroutine(endScreen());
         }
     }
 
+    public void TurnOffTutorial()
+    {
+        rt.SetActive(false);
+        Lt.SetActive(false);
+    }
+
     private IEnumerator endScreen()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("ThroneRoom");
     }
 
